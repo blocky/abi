95e1a1555ee9e39436acd55df545cb6b19791b3f
Sahil Sojitra <88416181+Sahil-4555@users.noreply.github.com>
2025-11-26 02:15:22 +0530

    Improve ABI uint64 encode/decode handling (#7)
    * refactor abi methods
    
    * added benchmark tests
    
    * fix lint
    
    * Use isNonZero function
    
    Manually inlining the isNonZero function
    and direcltly accessing the data in the slice does
    not significantly improve performance.
    
    **before**
    cpu: Intel(R) Core(TM) i7-10610U CPU @ 1.80GHz
    BenchmarkDecodeUint64/Small-8           76953645                13.78 ns/op
    BenchmarkDecodeUint64/Medium-8          88552545                13.69 ns/op
    BenchmarkDecodeUint64/Large-8           88805233                13.69 ns/op
    
    **after**
    cpu: Intel(R) Core(TM) i7-10610U CPU @ 1.80GHz
    BenchmarkDecodeUint64/Small-8           83156936                13.66 ns/op
    BenchmarkDecodeUint64/Medium-8          88754218                13.70 ns/op
    BenchmarkDecodeUint64/Large-8           87638418                13.70 ns/op
    
    * Use switch case over if chain
    
    The effective go docs suggests using case over if chain
    see [effective go switch
    section](https://go.dev/doc/effective_go#switch)
    for details.
    
    Updating to conform does not significantly effect performance.
    
    **before**
    BenchmarkDecodeSliceOfBytes/Small-3-items-8              4027724               279.6 ns/op
    BenchmarkDecodeSliceOfBytes/Medium-10-items-8            1799743               667.6 ns/op
    BenchmarkDecodeSliceOfBytes/Large-100-items-8             182762              6008 ns/op
    
    **after**
    cpu: Intel(R) Core(TM) i7-10610U CPU @ 1.80GHz
    BenchmarkDecodeSliceOfBytes/Small-3-items-8              4080576               277.9 ns/op
    BenchmarkDecodeSliceOfBytes/Medium-10-items-8            1816622               658.5 ns/op
    BenchmarkDecodeSliceOfBytes/Large-100-items-8             184166              5990 ns/op
    
    ---------
    
    Co-authored-by: David Millman <dave@cs.unc.edu>

---
+++ abi_bechmark_test.go
@@ -0,0 +1,287 @@
+package abi
+
+import (
+	"bytes"
+	"testing"
+)
+
+func BenchmarkEncodeUint64(b *testing.B) {
+	cases := []struct {
+		name string
+		v    uint64
+	}{
+		{"Small", 1},
+		{"Medium", 123456789},
+		{"Large", 1<<63 - 1},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			for b.Loop() {
+				_ = EncodeUint64(tc.v)
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeUint64(b *testing.B) {
+	cases := []struct {
+		name string
+		data []byte
+	}{
+		{"Small", EncodeUint64(1)},
+		{"Medium", EncodeUint64(123456789)},
+		{"Large", EncodeUint64(1<<63 - 1)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			for b.Loop() {
+				_, _ = DecodeUint64(tc.data)
+			}
+		})
+	}
+}
+
+func BenchmarkEncodeSliceOfBytes(b *testing.B) {
+	cases := []struct {
+		name string
+		data [][]byte
+	}{
+		{
+			"Small-3-items",
+			[][]byte{
+				[]byte("a"),
+				[]byte("bb"),
+				[]byte("ccc"),
+			},
+		},
+		{
+			"Medium-10-items",
+			func() [][]byte {
+				out := make([][]byte, 10)
+				for i := range out {
+					out[i] = bytes.Repeat([]byte{1}, 32)
+				}
+				return out
+			}(),
+		},
+		{
+			"Large-100-items",
+			func() [][]byte {
+				out := make([][]byte, 100)
+				for i := range out {
+					out[i] = bytes.Repeat([]byte{2}, 64)
+				}
+				return out
+			}(),
+		},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			for b.Loop() {
+				_, _ = EncodeSliceOfBytes(tc.data)
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeSliceOfBytes(b *testing.B) {
+	makeEnc := func(size, itemSize int) []byte {
+		arr := make([][]byte, size)
+		for i := range arr {
+			arr[i] = bytes.Repeat([]byte{1}, itemSize)
+		}
+		enc, _ := EncodeSliceOfBytes(arr)
+		return enc
+	}
+
+	cases := []struct {
+		name string
+		data []byte
+	}{
+		{"Small-3-items", makeEnc(3, 8)},
+		{"Medium-10-items", makeEnc(10, 32)},
+		{"Large-100-items", makeEnc(100, 64)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, _ = DecodeSliceOfBytes(tc.data)
+			}
+		})
+	}
+}
+
+// Helper to generate a tuple of n uint64 elements
+func makeUint64Tuple(n int) []EncoderFunc {
+	tuple := make([]EncoderFunc, n)
+	for i := 0; i < n; i++ {
+		v := uint64(i + 1)
+		tuple[i] = func() (EncoderResult, error) {
+			return EncoderResult{data: EncodeUint64(v)}, nil
+		}
+	}
+	return tuple
+}
+
+func BenchmarkEncodeTuple(b *testing.B) {
+	cases := []struct {
+		name      string
+		numFields int
+	}{
+		{"Small-3-elements", 3},
+		{"Medium-10-elements", 10},
+		{"Large-50-elements", 50},
+		{"VeryLarge-100-elements", 100},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			tuple := makeUint64Tuple(tc.numFields)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_, err := EncodeTuple(tuple...)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeTuple(b *testing.B) {
+	makeEncoded := func(n int) []byte {
+		encs := make([]EncoderFunc, n)
+		for i := range n {
+			encs[i] = EncodeTupleFuncUint64(uint64(i + 1))
+		}
+		out, _ := EncodeTuple(encs...)
+		return out
+	}
+
+	cases := []struct {
+		name string
+		data []byte
+	}{
+		{"Small-3-elements", makeEncoded(3)},
+		{"Medium-10-elements", makeEncoded(10)},
+		{"Large-50-elements", makeEncoded(50)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			dec := make([]DecoderFunc, len(tc.data)/32)
+			for i := range dec {
+				var v uint64
+				dec[i] = DecodeTupleFuncUint64(&v)
+			}
+			b.ResetTimer()
+
+			for b.Loop() {
+				_ = DecodeTuple(tc.data, dec...)
+			}
+		})
+	}
+}
+
+func BenchmarkEncodeTupleFuncUint64(b *testing.B) {
+	cases := []struct {
+		name string
+		v    uint64
+	}{
+		{"Small", 1},
+		{"Medium", 123456},
+		{"Large", 1<<63 - 1},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			f := EncodeTupleFuncUint64(tc.v)
+			b.ResetTimer()
+
+			for b.Loop() {
+				_, _ = f()
+			}
+		})
+	}
+}
+
+func BenchmarkEncodeTupleFuncBytes(b *testing.B) {
+	cases := []struct {
+		name string
+		data []byte
+	}{
+		{"Small-8B", bytes.Repeat([]byte{1}, 8)},
+		{"Medium-32B", bytes.Repeat([]byte{2}, 32)},
+		{"Large-128B", bytes.Repeat([]byte{3}, 128)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			f := EncodeTupleFuncBytes(tc.data)
+			b.ResetTimer()
+
+			for b.Loop() {
+				_, _ = f()
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeTupleFuncUint64(b *testing.B) {
+	cases := []struct {
+		name string
+		data []byte
+	}{
+		{"Small", EncodeUint64(1)},
+		{"Medium", EncodeUint64(123456789)},
+		{"Large", EncodeUint64(1<<63 - 1)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			var out uint64
+			f := DecodeTupleFuncUint64(&out)
+			full := tc.data
+			b.ResetTimer()
+
+			for b.Loop() {
+				_ = f(tc.data, full)
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeTupleFuncBytes(b *testing.B) {
+	makeEncoded := func(v []byte) (cur, full []byte) {
+		enc := EncodeTupleFuncBytes(v)
+		full, _ = EncodeTuple(enc)
+		cur = full[:32]
+		return
+	}
+
+	cases := []struct {
+		name string
+		data []byte
+	}{
+		{"Small-8B", bytes.Repeat([]byte{1}, 8)},
+		{"Medium-32B", bytes.Repeat([]byte{2}, 32)},
+		{"Large-128B", bytes.Repeat([]byte{3}, 128)},
+	}
+
+	for _, tc := range cases {
+		b.Run(tc.name, func(b *testing.B) {
+			var out []byte
+			cur, full := makeEncoded(tc.data)
+			f := DecodeTupleFuncBytes(&out)
+			b.ResetTimer()
+
+			for b.Loop() {
+				_ = f(cur, full)
+			}
+		})
+	}
+}
